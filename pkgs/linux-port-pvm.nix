da7f09a6fb21211352d379b7acbba0f00159d3f5
Alex Kwiatkowski <alex+git@fremantle.io>
2026-04-03 11:51:37 -0700

    fix: use checked-in aws pvm kernel config

--- pkgs/linux-port-pvm.nix
+++ pkgs/linux-port-pvm.nix
@@ -1,23 +1,50 @@
 {
-  lib,
   linux-pvm-src,
-  linux_6_12,
+  linuxPackages_custom,
+  stdenv,
   ...
 }:
-(
-  linux_6_12.override {
-    argsOverride = {
+let
+  version = "6.12.33";
+  modDirVersion = version;
+  configfile = ../configs/linux-port-pvm-x86_64.config;
+
+  kernel =
+    (linuxPackages_custom {
+      inherit version modDirVersion;
       src = linux-pvm-src;
-      version = "6.12.33";
-      modDirVersion = "6.12.33";
-      ignoreConfigErrors = true;
-      structuredExtraConfig = with lib.kernel; {
-        RUST = lib.mkForce no;
-        KVM_SW_PROTECTED_VM = yes;
-        KVM_PVM = module;
-      };
-    };
-  }
-).overrideAttrs (_previous: {
+      inherit configfile;
+    }).kernel;
+in
+kernel.overrideAttrs (previous: {
   enableParallelBuilding = false;
+  configurePhase = ''
+    runHook preConfigure
+
+    rm -rf /tmp/linux-port-pvm-build
+    mkdir -p /tmp/linux-port-pvm-build
+    export buildRoot="/tmp/linux-port-pvm-build"
+
+    echo "manual-config configurePhase buildRoot=$buildRoot pwd=$PWD"
+
+    if [ -f "$buildRoot/.config" ]; then
+      echo "Could not link $buildRoot/.config : file exists"
+      exit 1
+    fi
+    ln -sv ${configfile} $buildRoot/.config
+
+    make "''${makeFlags[@]}" oldconfig
+    runHook postConfigure
+
+    make "''${makeFlags[@]}" prepare
+    actualModDirVersion="$(cat $buildRoot/include/config/kernel.release)"
+    if [ "$actualModDirVersion" != "${modDirVersion}" ]; then
+      echo "Error: modDirVersion ${modDirVersion} specified in the Nix expression is wrong, it should be: $actualModDirVersion"
+      exit 1
+    fi
+
+    buildFlags+=("KBUILD_BUILD_TIMESTAMP=$(date -u -d @$SOURCE_DATE_EPOCH)")
+
+    cd $buildRoot
+  '';
 })
