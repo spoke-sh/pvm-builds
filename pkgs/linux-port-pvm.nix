a384f15fff9f40d84c8bf9b1e4a292cb38ac649a
Alex Kwiatkowski <alex+git@fremantle.io>
2026-04-03 08:50:24 -0700

    fix: make aws pvm kernel config tolerant

--- pkgs/linux-port-pvm.nix
+++ pkgs/linux-port-pvm.nix
@@ -9,7 +9,9 @@ linux_6_12.override {
     src = linux-pvm-src;
     version = "6.12.33";
     modDirVersion = "6.12.33";
+    ignoreConfigErrors = true;
     structuredExtraConfig = with lib.kernel; {
+      RUST = lib.mkForce no;
       KVM_SW_PROTECTED_VM = yes;
       KVM_PVM = module;
     };
