c0aa20dbe3d4bdcc05741b29e5122d1e4e699a15
Alex Kwiatkowski <alex+git@fremantle.io>
2026-04-03 09:02:58 -0700

    fix: disable parallel aws pvm kernel builds

--- pkgs/linux-port-pvm.nix
+++ pkgs/linux-port-pvm.nix
@@ -4,16 +4,20 @@
   linux_6_12,
   ...
 }:
-linux_6_12.override {
-  argsOverride = {
-    src = linux-pvm-src;
-    version = "6.12.33";
-    modDirVersion = "6.12.33";
-    ignoreConfigErrors = true;
-    structuredExtraConfig = with lib.kernel; {
-      RUST = lib.mkForce no;
-      KVM_SW_PROTECTED_VM = yes;
-      KVM_PVM = module;
+(
+  linux_6_12.override {
+    argsOverride = {
+      src = linux-pvm-src;
+      version = "6.12.33";
+      modDirVersion = "6.12.33";
+      ignoreConfigErrors = true;
+      structuredExtraConfig = with lib.kernel; {
+        RUST = lib.mkForce no;
+        KVM_SW_PROTECTED_VM = yes;
+        KVM_PVM = module;
+      };
     };
-  };
-}
+  }
+).overrideAttrs (_previous: {
+  enableParallelBuilding = false;
+})
