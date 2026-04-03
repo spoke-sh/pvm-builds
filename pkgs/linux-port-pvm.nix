444d2c612a82e8899425be23285c8349c665bea8
Alex Kwiatkowski <alex+git@fremantle.io>
2026-04-03 14:14:03 -0700

    fix: restore kernel install path semantics

--- pkgs/linux-port-pvm.nix
+++ pkgs/linux-port-pvm.nix
@@ -16,35 +16,6 @@ let
       inherit configfile;
     }).kernel;
 in
-kernel.overrideAttrs (previous: {
+kernel.overrideAttrs (_previous: {
   enableParallelBuilding = false;
-  configurePhase = ''
-    runHook preConfigure
-
-    rm -rf /tmp/linux-port-pvm-build
-    mkdir -p /tmp/linux-port-pvm-build
-    export buildRoot="/tmp/linux-port-pvm-build"
-
-    echo "manual-config configurePhase buildRoot=$buildRoot pwd=$PWD"
-
-    if [ -f "$buildRoot/.config" ]; then
-      echo "Could not link $buildRoot/.config : file exists"
-      exit 1
-    fi
-    ln -sv ${configfile} $buildRoot/.config
-
-    make "''${makeFlags[@]}" oldconfig
-    runHook postConfigure
-
-    make "''${makeFlags[@]}" prepare
-    actualModDirVersion="$(cat $buildRoot/include/config/kernel.release)"
-    if [ "$actualModDirVersion" != "${modDirVersion}" ]; then
-      echo "Error: modDirVersion ${modDirVersion} specified in the Nix expression is wrong, it should be: $actualModDirVersion"
-      exit 1
-    fi
-
-    buildFlags+=("KBUILD_BUILD_TIMESTAMP=$(date -u -d @$SOURCE_DATE_EPOCH)")
-
-    cd $buildRoot
-  '';
 })
